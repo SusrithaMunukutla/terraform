f2a55c9952a830fd5608102e96fbd25d48454929
SusrithaMunukutla <saim@virtusa.com>
2017-08-13 20:20:22 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,18 +2,19 @@
 # Variable declaration for AWS
 ##
 
-variable "aws_access_key" {}
+variable "aws_access_key" { default = "gahasgaha"}
 
-variable "aws_secret_key" {}
+variable "aws_secret_key" { default = "sFgaesuhqadnadhaaaaaa"}
 
-variable "aws_key_name" {
-  default = "The key name that should be used for the instance"
-}
+#variable "aws_key_name" {
+ # default = "The key name that should be used for the instance"
+#}
 
-variable "aws_account" {
-  description = "Account Number"
-}
+#variable "aws_account" {
+ # description = "Account Number"
+#}
 
 variable "aws_region" {
-  description = "Hive aws region"
+  description = "aws region"
+  default = "us-east-1"
 }
